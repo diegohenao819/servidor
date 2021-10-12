4396dcbf9d3b0bc378cb4e9da64b570b5e7fb1aa
diegohenao819 <diegohenao819@gmail.com>
2021-10-11 19:21:22 -0500

    DiegoDB

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://Nota-nueva:misiontic@cluster0.fxsz7.mongodb.net/DiegoDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("DiegoDB").collection("notas");
   // perform actions on the collection object
   client.close();
 });
